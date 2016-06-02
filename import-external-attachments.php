538370c4a0a3ae3ec524b596c065ca915a3e479b
Milo Jennings <milo@roadsidemultimedia.com>
2016-06-02 00:58:12 -0700

    Fixed post object path.

--- import-external-attachments.php
+++ import-external-attachments.php
@@ -426,7 +426,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 		$posts = new WP_Query($args);
 
-		return $posts;
+		return $posts->posts;
 	}
 	function external_image_get_post_type_list(){
 		$args = array(
